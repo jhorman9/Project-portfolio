54394a74d6a7e3be75a7f2df177f30e713ded911
jhorman9 <jhosmita9@gmail.com>
2022-08-09 00:00:58 -0500

    portfolio

---
+++ Portafolio 7/js/app.js
@@ -0,0 +1,27 @@
+const navBg = document.querySelector(".nav-bg");
+const navToggle = document.querySelector(".nav-toggle");
+const body = document.querySelector("body");
+const list = document.querySelectorAll(".list");
+
+navToggle.addEventListener("click", ()=> {
+
+    if (navBg.style.display == "block"){
+        navBg.style.display = "none";
+    }else{
+        navBg.style.display = "block";
+    }
+})
+
+let recorrido = list.forEach(element =>{
+    element.addEventListener("click", ()=> {
+        if(element.style.display == "block"){
+            navBg.style.display = "none"
+        }else{
+            element.style.display = "block";
+        }
+    })
+    
+})
+
+
+
